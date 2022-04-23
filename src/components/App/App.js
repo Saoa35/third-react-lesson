3194032bc2a4c4300a0f9a210eb6f31915a1c6ed
Saoa35 <saoamah@gmail.com>
2022-04-23 22:50:02 +0200

    import state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,28 @@
+import StatefullComponentExample from '../StatefullComponentExample/StatefullComponentExample';
 import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <StatefullComponentExample/>
+    </div>
+  );
+}
 
 
 // const names = ['Alice', 'Kim', 'Julian', 'Alex', 'Tatiana'];
 // const arrElements = names.map(item => <li>{item}</li>);
 
-function App() {
-  return (
-    <div className="App">
-      {/* <ul>
-        {arrElements}
-      </ul> */}
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <ul>
+//         {arrElements}
+//       </ul>
+//     </div>
+//   );
+// }
 
 export default App;
