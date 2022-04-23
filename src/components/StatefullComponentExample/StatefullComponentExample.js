a448ead62417c78d21d642610aa598223c856472
Saoa35 <saoamah@gmail.com>
2022-04-23 22:49:40 +0200

    +statr obj

--- src/components/StatefullComponentExample/StatefullComponentExample.js
+++ src/components/StatefullComponentExample/StatefullComponentExample.js
@@ -2,6 +2,39 @@ import React from "react";
 
 class StatefullComponentExample extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state ={
+            counts: 0,
+            list: [1, 2, 3, 4, 5],
+            str: 'some string in state',
+            obj: {
+                key:'key1'
+            },
+            isShow: true
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>StatefullComponentExample</h2>
+
+                <button>
+                    Click me!
+                </button>
+
+                <h3>
+                    Clicked: {this.state.counts}
+                </h3>
+
+                <p>
+                    {this.state.str}
+                </p>
+            </div>
+        )
+    }
 }
 
 export default StatefullComponentExample;
