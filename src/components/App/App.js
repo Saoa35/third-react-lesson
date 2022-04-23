27e9dcf02afd8f72b32ac0b5c8121067e364a5a1
Saoa35 <saoamah@gmail.com>
2022-04-23 21:59:28 +0200

    +comments

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+
+// const names = ['Alice', 'Kim', 'Julian', 'Alex', 'Tatiana'];
+// const arrElements = names.map(item => <li>{item}</li>);
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ul>
+        {arrElements}
+      </ul> */}
+    </div>
+  );
+}
+
+export default App;
